7d69d3b1f13acefe7de2cbae09d7966cc848440c
baglaanugrah <e1471265@u.nus.edu>
2026-01-23 00:19:04 +0800

    Add store and display feature

--- src/main/java/Zip.java
+++ src/main/java/Zip.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Zip {
+
+    private static List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -19,8 +24,15 @@ public class Zip {
                 System.out.println(" Bye. Hope to see you again soon!");
                 printLine();
                 break;
+            } else if (input.equalsIgnoreCase("list"))  {
+                int i = 1;
+                for (String s : Zip.list) {
+                    System.out.println(i + "." + " " + s);
+                    i++;
+                }
             } else {
-                System.out.println(" " + input);
+                System.out.println(" " + "added: " + input);
+                Zip.list.add(input);
                 printLine();
             }
         }
