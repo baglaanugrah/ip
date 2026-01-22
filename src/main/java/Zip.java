a41edd0595454f4be67f6307197c9c63a7c49e7d
baglaanugrah <e1471265@u.nus.edu>
2026-01-23 00:43:41 +0800

    Add task status

--- src/main/java/Zip.java
+++ src/main/java/Zip.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Zip {
 
-    private static List<String> list = new ArrayList<>();
+    private static List<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -24,15 +24,41 @@ public class Zip {
                 System.out.println(" Bye. Hope to see you again soon!");
                 printLine();
                 break;
-            } else if (input.equalsIgnoreCase("list"))  {
+            } else if (input.equalsIgnoreCase("list")) {
                 int i = 1;
-                for (String s : Zip.list) {
-                    System.out.println(i + "." + " " + s);
+                if (Zip.list.isEmpty()) {
+                    System.out.println("There are no tasks in the list");
+                }
+                for (Task s : Zip.list) {
+                    System.out.println(i + "." + " " + "[" + s.getStatusIcon() + "]" + " " + s.getDescription());
                     i++;
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+
+                if (index >= 0 && index < Zip.list.size()) {
+                    Zip.list.get(index).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[X] " + Zip.list.get(index).getDescription());
+
+                } else {
+                    System.out.println(" Invalid task number.");
+                }
+
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+
+                if (index >= 0 && index < Zip.list.size()) {
+                    Zip.list.get(index).markAsUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[] " + Zip.list.get(index).getDescription());
+                } else {
+                    System.out.println(" Invalid task number.");
+                }
             } else {
+                Task t = new Task(input);
                 System.out.println(" " + "added: " + input);
-                Zip.list.add(input);
+                Zip.list.add(t);
                 printLine();
             }
         }
