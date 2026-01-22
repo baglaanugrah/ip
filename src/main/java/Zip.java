c3bc112b901b536da41fd360493be0dbef438293
baglaanugrah <e1471265@u.nus.edu>
2026-01-22 23:55:20 +0800

    Add echo feature

--- src/main/java/Zip.java
+++ src/main/java/Zip.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class Zip {
     public static void main(String[] args) {
-        String logo =
-                  "------------------------------------------------------------\n"
-                + "Hello! I'm Zip\n"
-                + "What can I do for you?\n"
-                + "\n"
-                + "------------------------------------------------------------\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "\n"
-                + "------------------------------------------------------------";
-        System.out.println("\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        printLine();
+        System.out.println(" Hello! I'm Zip");
+        System.out.println(" What can I do for you?");
+        printLine();
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+
+            printLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            } else {
+                System.out.println(" " + input);
+                printLine();
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
+
