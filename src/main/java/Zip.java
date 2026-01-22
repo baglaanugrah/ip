b17edd534f1bbf8b23a661930f0c2005af6bb602
baglaanugrah <e1471265@u.nus.edu>
2026-01-22 22:11:55 +0800

    Add welcome message

---
+++ src/main/java/Zip.java
@@ -0,0 +1,14 @@
+public class Zip {
+    public static void main(String[] args) {
+        String logo =
+                  "------------------------------------------------------------\n"
+                + "Hello! I'm Zip\n"
+                + "What can I do for you?\n"
+                + "\n"
+                + "------------------------------------------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "\n"
+                + "------------------------------------------------------------";
+        System.out.println("\n" + logo);
+    }
+}
